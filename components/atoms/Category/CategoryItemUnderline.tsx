b734fb43726e50abee72e036235af8de28fe5536
nolec <nolec@emmental.co.kr>
2022-02-13 02:03:16 +0900

    feat: 카테고리 아이템 밑줄

---
+++ components/atoms/Category/CategoryItemUnderline.tsx
@@ -0,0 +1,7 @@
+import tw from 'twin.macro';
+
+const Underline = tw.div`w-full bg-[#000000] h-[4px]`;
+const CategoryItemUnderline = () => {
+  return <Underline />;
+};
+export default CategoryItemUnderline;
