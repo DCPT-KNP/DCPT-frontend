9c0e6554da791e284ac72aa0fdafe6651d2e4cd6
nolec <nolec@emmental.co.kr>
2022-02-27 15:39:46 +0900

    feat: logobtn 버튼 생성

--- components/layout/HeaderLayout.tsx
+++ components/layout/HeaderLayout.tsx
@@ -1,15 +1,20 @@
 import Logo from '@/img/icons/logo.svg';
+import { useRouter } from 'next/router';
 import tw from 'twin.macro';
 
 const Header = tw.header`sticky flex top-0 px-16 shadow-[0px 10px 30px #F0F0F0] bg-white z-10`;
-const LogoBox = tw.div``;
+const LogoBtn = tw.button``;
 
 const HeaderLayout = () => {
+  const router = useRouter();
+  const handleClickGoToHome = () => {
+    router.push('/');
+  };
   return (
     <Header>
-      <LogoBox>
+      <LogoBtn onClick={handleClickGoToHome}>
         <Logo src={Logo} alt={'logo'} />
-      </LogoBox>
+      </LogoBtn>
     </Header>
   );
 };
