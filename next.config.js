21fa4af41abd86c4e0153843b5786da1f3d79cb9
aiden <rlaalsrb3559@naver.com>
2022-03-01 16:17:32 +0900

    config: api key env 처리

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    clientId: 'efb2040825bd2bb9de28e4d9520be606',
+    redirectUrl: 'http://localhost:3000'
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
