5e67335a35665ba4b0edfb5f720fb0c5d013a36f
aiden <rlaalsrb3559@naver.com>
2022-03-01 22:46:21 +0900

    config: javascriptKey값 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     clientId: 'efb2040825bd2bb9de28e4d9520be606',
-    redirectUrl: 'http://localhost:3000'
+    redirectUrl: 'http://localhost:3000',
+    javascriptKakaoKey: '754e84ae19a3bd59463c6848efd2e58b'
+  },
+  async rewrites() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [
+        {
+          destination: process.env.DESTINATION_URL,
+          source: process.env.SOURCE_PATH
+        }
+      ];
+    }
   },
   webpack: (config) => {
     config.module.rules.push({
