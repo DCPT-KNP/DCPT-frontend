f53e6838c7ab585983a765680493211583faa6b9
aiden <rlaalsrb3559@naver.com>
2022-03-01 16:18:21 +0900

    feat: kakao login 인가코드 받는것까지 , add axios package

--- components/Buttons/IconBtn.tsx
+++ components/Buttons/IconBtn.tsx
@@ -3,10 +3,12 @@ import kakao from '@/img/icons/kakao.png';
 import naver from '@/img/icons/naver.png';
 import google from '@/img/icons/google.png';
 import Image from 'next/image';
-
+import axios from 'axios';
+import { KAKAO_AUTH_URL } from 'constants/social';
 interface IIconBtnProps {
   imageUrl: any;
   name: string;
+  onClick?: () => void;
 }
 
 const IconBtnBlock = tw.button`
@@ -15,15 +17,25 @@ const IconBtnBlock = tw.button`
 
 const IconImg = tw(Image)`rounded-full`;
 
-const IconBtn = ({ imageUrl, name }: IIconBtnProps) => {
+const IconBtn = ({ imageUrl, name, onClick }: IIconBtnProps) => {
   return (
-    <IconBtnBlock>
-      <IconImg src={imageUrl} alt={name} width={72} height={72} />
+    <IconBtnBlock onClick={onClick}>
+      <a href={KAKAO_AUTH_URL}>
+        <IconImg src={imageUrl} alt={name} width={72} height={72} />
+      </a>
     </IconBtnBlock>
   );
 };
 
 export const KakaoIconBtn = () => {
+  const kakaoLogin = async () => {
+    try {
+      const result = await axios.get(`${KAKAO_AUTH_URL}`);
+      console.log(result);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
   return <IconBtn imageUrl={kakao} name="카카오" />;
 };
 
---
+++ constants/social.ts
@@ -0,0 +1,1 @@
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.clientId}&redirect_uri=${process.env.redirectUrl}&response_type=code`;
