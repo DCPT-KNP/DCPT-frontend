b55e231c09ae9cb920007763f9a22723077194ef
aiden <rlaalsrb3559@naver.com>
2022-03-03 00:51:37 +0900

    config: baseUrl 수정

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://13.209.35.135:3000';
+export const BASE_URL = 'http://3.39.56.223:3000';
 
 export const key = {
   kakao: process.env.NEXT_PUBLIC_JAVASCRIPT_KAKAO_KEY
