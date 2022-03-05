3b632a0c421e261567d8b45df593b067933d0996
aiden <rlaalsrb3559@naver.com>
2022-03-06 01:12:24 +0900

    feat: 모든 get 요청에 getAxiosHeader default로 추가

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,6 @@
 import { BASE_URL } from 'constants/config';
 import axios, { AxiosResponse } from 'axios';
+import { getStorageItem } from 'hooks/useLocalStorage';
 
 const instance = axios.create({
   baseURL: BASE_URL
@@ -8,13 +9,21 @@ const instance = axios.create({
 const responseBody = (response: AxiosResponse) => response.data;
 
 export const requests = {
-  get: (url: string, params = {}) =>
-    instance
+  get: (url: string, params = {}) => {
+    instance.defaults.headers.common = getAxiosHeader();
+    return instance
       .get(url, {
         params
       })
-      .then(responseBody),
+      .then(responseBody);
+  },
   post: (url: string, body: {}) => instance.post(url, body).then(responseBody),
   put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
   delete: (url: string) => instance.delete(url).then(responseBody)
+};
+
+export const getAxiosHeader = () => {
+  const token = getStorageItem('accessToken');
+
+  return token ? { Authorization: `Bearer ${token}` } : { Authorization: `` };
 };
