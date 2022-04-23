bcc8f1235dd977400a1c97314e4469871628f15f
nolec <ca10205@naver.com>
2022-04-23 21:43:38 +0900

    fix: metadata 수정

--- metadata/selection-map.ts
+++ metadata/selection-map.ts
@@ -1,1 +1,66 @@
-export type SelectionMapType = 'i' | 't' | 'pi';
+// export type SelectionMapType = 'si' | 'bi' | 'st' | 'bt' | 'spi' | 'bpi';
+type ShapeType = 'i' | 't' | 'pi';
+type SizeType = 'short' | 'long';
+
+export type SelectionMapType = {
+  type: ShapeType;
+  initIndex: number;
+  box: SizeType[];
+};
+
+export const iShape: SelectionMapType = {
+  type: 'i',
+  initIndex: 3,
+  box: [
+    'short',
+    'short',
+    'short',
+    'long',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short'
+  ]
+};
+export const tShape: SelectionMapType = {
+  type: 't',
+  initIndex: 3,
+  box: [
+    'short',
+    'short',
+    'short',
+    'long',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short'
+  ]
+};
+export const piShape: SelectionMapType = {
+  type: 'pi',
+  initIndex: 2,
+  box: [
+    'short',
+    'short',
+    'long',
+    'short',
+    'long',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short',
+    'short'
+  ]
+};
